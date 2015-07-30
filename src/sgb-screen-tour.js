adce901fb195848ab0ec7aa9bb65011a0d44305c
luismiglietti <luismiglietti@gmail.com>
2015-07-30 13:53:06 -0430

    tour controller + template

---
+++ src/sgb-screen-tour.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('sgb-screen-tour', ['megazord'])
+    .controller('sgb-screen-tour-controller', ['$stateParams', '_screen', '_screenParams', '$scope', function ($stateParams, _screen, _screenParams, $scope) {
+
+        _screen.initialize($scope, _screenParams);
+        $scope.slides = $stateParams.data.slides;
+        $scope.goTo = _screenParams.goTo; 
+
+    }]);
