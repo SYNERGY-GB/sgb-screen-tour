c6fcf5a170f99a4536a3338820e2c473bde34d44
luismiglietti <luismiglietti@gmail.com>
2015-08-10 10:32:35 -0430

    added functionality

--- src/sgb-screen-tour.js
+++ src/sgb-screen-tour.js
@@ -1,10 +1,23 @@
 'use strict';
 
 angular.module('sgb-screen-tour', ['megazord'])
-    .controller('sgb-screen-tour-controller', ['$stateParams', '_screen', '_screenParams', '$scope', 
-    			function ($stateParams, _screen, _screenParams, $scope) {
+    .controller('sgb-screen-tour-controller', ['$stateParams', '_screen', '_screenParams', '$scope','$ionicSlideBoxDelegate', 
+    			function ($stateParams, _screen, _screenParams, $scope, $ionicSlideBoxDelegate) {
 
         _screen.initialize($scope, _screenParams);
         $scope.slides = $stateParams.data; 
         $scope.goTo = _screenParams.goTo; 
+        $scope.previousIcon = _screenParams.previousIcon; 
+        $scope.nextIcon = _screenParams.nextIcon; 
+        $scope.showPager = _screenParams.showProgress ; 
+
+        $scope.reduceImage = $scope.previousIcon && $scope.nextIcon; 
+        console.log($scope.reduceImage, "d"); 
+
+        $scope.next = function() {
+    		$ionicSlideBoxDelegate.next();
+  		};
+  		$scope.previous = function() {
+    		$ionicSlideBoxDelegate.previous();
+  		};
     }]);
