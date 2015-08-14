c36174509de3b3109485fd5b760767829d34164f
luismiglietti <luismiglietti@gmail.com>
2015-08-14 10:48:14 -0430

    changed template to use screen parameters directly

--- dist/sgb-screen-tour.js
+++ dist/sgb-screen-tour.js
@@ -5,16 +5,13 @@ angular.module('sgb-screen-tour', ['megazord'])
     .controller('sgb-screen-tour-controller', ['$stateParams', '_screen', '_screenParams', '$scope','$ionicSlideBoxDelegate', 
     			function ($stateParams, _screen, _screenParams, $scope, $ionicSlideBoxDelegate) {
 
+        //Obtain parameters from config file
         _screen.initialize($scope, _screenParams);
-        $scope.slides = $stateParams.data; 
-        $scope.previousIcon = _screenParams.previousIcon; 
-        $scope.nextIcon = _screenParams.nextIcon; 
 
-        //Show pager depending on user choice
-        $scope.showPager = _screenParams.showProgress;
+        $scope.slides = $stateParams.data; 
 
         //Show icons if both selected
-        $scope.reduceImage = $scope.previousIcon && $scope.nextIcon; 
+        $scope.reduceImage = $scope._screenParams.previousIcon && $scope._screenParams.nextIcon; 
 
         $scope.next = function() {
     		$ionicSlideBoxDelegate.next();
