5d2d8c71923fdc38554be938b5ef5868767d1fc3
luismiglietti <luismiglietti@gmail.com>
2015-08-19 10:31:59 -0430

    changed template and controller so pager is always under image

--- src/sgb-screen-tour.js
+++ src/sgb-screen-tour.js
@@ -7,15 +7,25 @@ angular.module('sgb-screen-tour', ['megazord'])
         //Obtain parameters from config file
         _screen.initialize($scope, _screenParams);
 
+        //Obtain screen data
         $scope.slides = $stateParams.data; 
 
         //Show icons if both selected
         $scope.reduceImage = $scope._screenParams.previousIcon && $scope._screenParams.nextIcon; 
 
+        //Current text is the first slide text
+        $scope.desc = $scope.slides[0]?$scope.slides[0].desc : undefined; 
+
+        //Change text when slide changes
+        $scope.changeText = function (slide) {
+          $scope.desc = $scope.slides[$ionicSlideBoxDelegate.currentIndex()].desc; 
+        };
+
         $scope.next = function() {
-    		$ionicSlideBoxDelegate.next();
-  		};
-  		$scope.previous = function() {
-    		$ionicSlideBoxDelegate.previous();
-  		};
+      		$ionicSlideBoxDelegate.next();
+    		};
+    		$scope.previous = function() {
+      		$ionicSlideBoxDelegate.previous();
+    		};
+
     }]);
